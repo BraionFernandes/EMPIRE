ad0d57be5db56d5cf80739fa2d0c5ca2ff5127eb
BraionFernandes <132958791+BraionFernandes@users.noreply.github.com>
2023-12-12 15:01:47 -0300

    pre finalização do site principal

--- WILDRIFT/js/script.js
+++ WILDRIFT/js/script.js
@@ -4,28 +4,74 @@ const rotaJungle=document.querySelector("#rotaJungle");
 const rotaMid=document.querySelector("#rotaMid");
 const rotaDuo=document.querySelector("#rotaDuo");
 const rotaSup=document.querySelector("#rotaSup");
-const champ=document.querySelectorAll(".conteudo-build-champ-item")
+const ALL=document.querySelectorAll(".conteudo-build-champ-item");
 const TOP=document.querySelectorAll(".TOP");
 const JUNGLE=document.querySelectorAll(".JUNGLE");
 const MID=document.querySelectorAll(".MID");
 const ADC=document.querySelectorAll(".ADC");
 const SUP=document.querySelectorAll(".SUP");
+const btnRotas=document.querySelectorAll(".conteudo-build-artigo-item");
+const divChamp=document.querySelector(".conteudo-build-champ");
 
-const champs=[champ]
-
+rotaAll.addEventListener("click",()=>{
+    btnRotas.forEach(el=>{
+        el.classList.remove("selecionado1")
+    })
+    rotaAll.classList.add("selecionado1")
+    divChamp.innerHTML=""
+    ALL.forEach(el=>{
+        divChamp.appendChild(el)
+    })
+})
 
 rotaTop.addEventListener("click",()=>{
-    
+    btnRotas.forEach(el=>{
+        el.classList.remove("selecionado1")
+    })
+    rotaTop.classList.add("selecionado1")
+    divChamp.innerHTML=""
+    TOP.forEach(el=>{
+        divChamp.appendChild(el)
+    })
 })
+
 rotaJungle.addEventListener("click",()=>{
-    
+    btnRotas.forEach(el=>{
+        el.classList.remove("selecionado1")
+    })
+    rotaJungle.classList.add("selecionado1")
+    divChamp.innerHTML=""
+    JUNGLE.forEach(el=>{
+        divChamp.appendChild(el)
+    })
 })
 rotaMid.addEventListener("click",()=>{
-    
+    btnRotas.forEach(el=>{
+        el.classList.remove("selecionado1")
+    })
+    rotaMid.classList.add("selecionado1")
+    divChamp.innerHTML=""
+    MID.forEach(el=>{
+        divChamp.appendChild(el)
+    })
 })
 rotaDuo.addEventListener("click",()=>{
-    
+    btnRotas.forEach(el=>{
+        el.classList.remove("selecionado1")
+    })
+    rotaDuo.classList.add("selecionado1")
+    divChamp.innerHTML=""
+    ADC.forEach(el=>{
+        divChamp.appendChild(el)
+    })
 })
 rotaSup.addEventListener("click",()=>{
-    
+    btnRotas.forEach(el=>{
+        el.classList.remove("selecionado1")
+    })
+    rotaSup.classList.add("selecionado1")
+    divChamp.innerHTML=""
+    SUP.forEach(el=>{
+        divChamp.appendChild(el)
+    })
 })
