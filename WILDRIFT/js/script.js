1805300f4c83240a12effc402915338df1509ac5
BraionFernandes <132958791+BraionFernandes@users.noreply.github.com>
2023-12-12 13:09:07 -0300

    continuação do projeto wild rift

---
+++ WILDRIFT/js/script.js
@@ -0,0 +1,31 @@
+const rotaAll=document.querySelector("#rotaAll");
+const rotaTop=document.querySelector("#rotaTop");
+const rotaJungle=document.querySelector("#rotaJungle");
+const rotaMid=document.querySelector("#rotaMid");
+const rotaDuo=document.querySelector("#rotaDuo");
+const rotaSup=document.querySelector("#rotaSup");
+const champ=document.querySelectorAll(".conteudo-build-champ-item")
+const TOP=document.querySelectorAll(".TOP");
+const JUNGLE=document.querySelectorAll(".JUNGLE");
+const MID=document.querySelectorAll(".MID");
+const ADC=document.querySelectorAll(".ADC");
+const SUP=document.querySelectorAll(".SUP");
+
+const champs=[champ]
+
+
+rotaTop.addEventListener("click",()=>{
+    
+})
+rotaJungle.addEventListener("click",()=>{
+    
+})
+rotaMid.addEventListener("click",()=>{
+    
+})
+rotaDuo.addEventListener("click",()=>{
+    
+})
+rotaSup.addEventListener("click",()=>{
+    
+})
